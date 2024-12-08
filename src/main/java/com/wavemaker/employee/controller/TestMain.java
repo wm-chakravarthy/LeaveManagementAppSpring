2d7e17b0e17f43a0ad2c54730ca7d5062c08b24e
chakravarthyBatna <chakravarthybatna46234@gmail.com>
2024-12-08 10:48:46 +0530

    commit from branch 2

--- src/main/java/com/wavemaker/employee/controller/TestMain.java
+++ src/main/java/com/wavemaker/employee/controller/TestMain.java
@@ -3,6 +3,8 @@ package com.wavemaker.employee.controller;
 import java.util.ArrayList;
 import java.util.List;
 //this is main branch commit
+//commit from branch 2
+
 public class TestMain {
     public static void main(String[] args) {
         List<Dummy> dummyList = new ArrayList<>(10);
